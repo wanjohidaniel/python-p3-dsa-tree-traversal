96f71b6ae4f9cf97e6dc8936033f042b28049f99
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-06-05 15:46:14 +0300

    Tree Traversal

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        return self._dfs(self.root, id)
+
+    def _dfs(self, node, id):
+        if not node:
+            return None
+        if node['id'] == id:
+            return node
+        for child in node['children']:
+            result = self._dfs(child, id)
+            if result:
+                return result
+        return None
